e832daad409c72e5dd246e584b00a2f3876d17e8
Muhammad Fayyaz <muhammad.fayyaz.se@gmail.com>
2023-06-16 00:08:53 +0400

    changed swagger version 2.4.0 to 2.8.0

--- src/main/java/com/mobile/SwaggersConfiguration.java
+++ src/main/java/com/mobile/SwaggersConfiguration.java
@@ -15,7 +15,7 @@ import java.util.function.Predicate;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
-@EnableWebMvc
+//@EnableWebMvc
 @EnableSwagger2
 @Configuration
 public class SwaggersConfiguration {
@@ -28,24 +28,6 @@ public class SwaggersConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
-    }
-
-//    @Bean
-//    public Docket postsApi() {
-//        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
-//                .apiInfo(apiInfo()).select().paths(postPaths()).build();
-//    }
-//
-//    private Predicate<String> postPaths() {
-//        return or(regex("/api/posts.*"), regex("/api/javainuse.*"));
-//    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("JavaInUse API")
-                .description("JavaInUse API reference for developers")
-                .termsOfServiceUrl("http://javainuse.com")
-                .contact("javainuse@gmail.com").license("JavaInUse License")
-                .licenseUrl("javainuse@gmail.com").version("1.0").build();
     }
 
 }
