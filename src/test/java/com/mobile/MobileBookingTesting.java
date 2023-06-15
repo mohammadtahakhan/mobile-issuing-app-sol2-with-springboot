15191c64bca66a29e00e2bd5e326d53718e4346d
mohammadtahakhan <sahilkhanlove@rediffmail.com>
2023-06-15 23:33:52 +0400

    Added test cases

---
+++ src/test/java/com/mobile/MobileBookingTesting.java
@@ -0,0 +1,30 @@
+package com.mobile;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class MobileBookingTesting {
+
+        private MobilePhoneBookingService booking;
+        private BookedBy user1;
+        private BookedBy user2;
+
+        @BeforeEach
+        public void setUp() {
+            booking = new MobilePhoneBookingService();
+            user1 = new BookedBy("User 1");
+            user2 = new BookedBy("User 2");
+        }
+
+        @Test
+        public void testBookPhone() {
+            String bookedPhoneId = booking.bookPhone("Samsung Galaxy S8", user1);
+            MobilePhone phone = booking.getAvailablePhoneById(UUID.fromString(bookedPhoneId));
+            Assertions.assertNotNull(phone);
+            Assertions.assertFalse(phone.isAvailable());
+            Assertions.assertEquals(user1, phone.getBookedBy());
+        }
+}
